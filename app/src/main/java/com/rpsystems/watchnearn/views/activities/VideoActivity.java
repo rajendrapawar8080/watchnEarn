1a86e378a582b7d6d5f20bb58cacdeecd9ecfc09
neosoftrajendra <rajendra.pawar@neosofttech.com>
2017-03-20 19:51:02 +0530

    add mod on videos integration

---
+++ app/src/main/java/com/rpsystems/watchnearn/views/activities/VideoActivity.java
@@ -0,0 +1,158 @@
+package com.rpsystems.watchnearn.views.activities;
+
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.rpsystems.watchnearn.Manifest;
+import com.rpsystems.watchnearn.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by neosoft on 20/3/17.
+ */
+
+public class VideoActivity extends AppCompatActivity {
+    public static String TAG = "VideoViewActivity";
+    private static final int REQUEST_WRITE_STORAGE = 112;
+    @BindView(R.id.videoView_ID) VideoView mVideoView;
+    private InterstitialAd mInterstetialAdds;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_videoview);
+        ButterKnife.bind(this);
+
+        checkDevice();
+        Log.d(TAG, "external storage path=" + Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath()+"/Sample Videos.mp4");
+
+    }
+    private void initMediaController() {
+        MediaController mediaController = new MediaController(VideoActivity.this);
+        mediaController.setAnchorView(mVideoView);
+        Uri uri = Uri.parse(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath()+"/Sample Videos.mp4");
+        mVideoView.setMediaController(mediaController);
+        mVideoView.setVideoURI(uri);
+        mVideoView.requestFocus();
+        mVideoView.start();
+        Log.d(TAG, "external storage path=" + Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath()+"/Sample Videos.mp4");
+        initAdds();
+        /*if (mInterstetialAdds.isLoaded()){
+            mInterstetialAdds.show();
+        }*/
+
+
+    }
+    private void checkDevice(){
+        if (Build.VERSION.SDK_INT>=android.os.Build.VERSION_CODES.M){
+            checkPermission();
+        }else{
+            initMediaController();
+
+        }
+
+    }
+    private void initAdds(){
+        mInterstetialAdds=new InterstitialAd(VideoActivity.this);
+        mInterstetialAdds.setAdUnitId("ca-app-pub-4371432322602969/3249622334");
+        mInterstetialAdds.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                requestNewInterstitial();
+                // beginPlayingGame();
+            }
+
+            @Override
+            public void onAdLoaded() {
+                super.onAdLoaded();
+                mInterstetialAdds.show();
+            }
+        });
+        requestNewInterstitial();
+    }
+    private void checkPermission() {
+        //ask for the permission in android M
+        int permission = ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "Permission to record denied");
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage("Permission to access the SD-CARD is required for this app to Download PDF.")
+                        .setTitle("Permission required");
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int id) {
+                        Log.i(TAG, "Clicked");
+                        makeRequest();
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+            } else {
+                makeRequest();
+            }
+        }else{
+            initMediaController();
+        }
+    }
+
+    protected void makeRequest() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE},
+                REQUEST_WRITE_STORAGE);
+    }
+    private void requestNewInterstitial() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+
+        mInterstetialAdds.loadAd(adRequest);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_WRITE_STORAGE: {
+
+                if (grantResults.length == 0
+                        || grantResults[0] !=
+                        PackageManager.PERMISSION_GRANTED) {
+
+                    Log.i(TAG, "Permission has been denied by user");
+
+                } else {
+                    initMediaController();
+                    Log.i(TAG, "Permission has been granted by user");
+
+                }
+                return;
+            }
+        }
+    }
+}
--- app/src/main/java/com/rpsystems/watchnearn/views/fragments/OffersWallFragment.java
+++ app/src/main/java/com/rpsystems/watchnearn/views/fragments/OffersWallFragment.java
@@ -1,11 +1,15 @@
 package com.rpsystems.watchnearn.views.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
@@ -14,14 +18,22 @@ import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.InterstitialAd;
 import com.rpsystems.watchnearn.R;
+import com.rpsystems.watchnearn.views.activities.VideoActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 //Our class extending fragment
 public class OffersWallFragment extends Fragment {
-
-    private VideoView mVideoFirst;
+    @BindView(R.id.videoOne_ID) ImageView mVideoFirst;
+    @BindView(R.id.videoTwo_ID) ImageView mVideoSecond;
+    @BindView(R.id.videoThree_ID)ImageView mVideoThird;
+    @BindView(R.id.videoFour_ID) ImageView mVideoFour;
+    @BindView(R.id.videoFive_ID) ImageView mVideoFive;
+    @BindView(R.id.videoSix_ID) ImageView mVideoSix;
     private View mView;
     private InterstitialAd mInterstitialAd;
-    TextView textView;
 
     //Overriden method onCreateView
     public OffersWallFragment(){
@@ -32,13 +44,14 @@ public class OffersWallFragment extends Fragment {
 
         //Returning the layout file after inflating
         //Change R.layout.fragment_payment in you classes
-        mView=inflater.inflate(R.layout.fragmentofferswall,container,false);
+        mView=inflater.inflate(R.layout.fragment_offers,container,false);
+        ButterKnife.bind(this,mView);
         initViews(mView);
-        initListener();
+
         return mView;
     }
    private void initViews(View view){
-       mVideoFirst= (VideoView)view.findViewById(R.id.videoViewOne_Id);
+       mVideoFirst= (ImageView)view.findViewById(R.id.videoOne_ID);
 
        mInterstitialAd = new InterstitialAd(getActivity());
        mInterstitialAd.setAdUnitId("ca-app-pub-4371432322602969/3249622334");
@@ -51,28 +64,49 @@ public class OffersWallFragment extends Fragment {
        });
 
        requestNewInterstitial();
-
-       textView= (TextView) view.findViewById(R.id.textView);
-       textView.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
-               if (mInterstitialAd.isLoaded()) {
-                   mInterstitialAd.show();
-               }else {
-                   Toast.makeText(getActivity(), "Ad did not load", Toast.LENGTH_SHORT).show();
-               }
-           }
-       });
-
-
    }
-    private void initListener(){
-        mVideoFirst.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
+   @OnClick(R.id.videoOne_ID) public void displayAdd(){
+       newActivity();
+     /*  if (mInterstitialAd.isLoaded()) {
+           mInterstitialAd.show();
+       }else {
+           Toast.makeText(getActivity(), "Ad did not load", Toast.LENGTH_SHORT).show();
+       }*/
+   }
+    @OnClick(R.id.videoTwo_ID) public void displayAddTwo(){
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        }else {
+            Toast.makeText(getActivity(), "Ad did not load", Toast.LENGTH_SHORT).show();
+        }
+    }
+    @OnClick(R.id.videoThree_ID) public void displayAddThree(){
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        }else {
+            Toast.makeText(getActivity(), "Ad did not load", Toast.LENGTH_SHORT).show();
+        }
+    }
+    @OnClick(R.id.videoFour_ID) public void displayAddFour(){
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        }else {
+            Toast.makeText(getActivity(), "Ad did not load", Toast.LENGTH_SHORT).show();
+        }
+    }
+    @OnClick(R.id.videoFive_ID) public void displayAddFive(){
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        }else {
+            Toast.makeText(getActivity(), "Ad did not load", Toast.LENGTH_SHORT).show();
+        }
+    }
+    @OnClick(R.id.videoSix_ID) public void displayAddSix(){
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        }else {
+            Toast.makeText(getActivity(), "Ad did not load", Toast.LENGTH_SHORT).show();
+        }
     }
     private void requestNewInterstitial() {
         AdRequest adRequest = new AdRequest.Builder()
@@ -80,5 +114,8 @@ public class OffersWallFragment extends Fragment {
                 .build();
 
         mInterstitialAd.loadAd(adRequest);
+    }
+    public void newActivity(){
+        startActivity(new Intent(getActivity(), VideoActivity.class));
     }
 }
