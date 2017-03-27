a5bf1246333a4b9070b1d3ca1b757f8c99767bf0
neosoftrajendra <rajendra.pawar@neosofttech.com>
2017-03-27 17:16:05 +0530

    login and sign up screen designing

---
+++ app/src/main/java/com/rpsystems/watchnearn/views/activities/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.rpsystems.watchnearn.views.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.rpsystems.watchnearn.R;
+
+/**
+ * Created by neosoft on 27/3/17.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
---
+++ app/src/main/java/com/rpsystems/watchnearn/views/activities/SignUpActivity.java
@@ -0,0 +1,37 @@
+package com.rpsystems.watchnearn.views.activities;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.rpsystems.watchnearn.R;
+
+/**
+ * Created by neosoft on 27/3/17.
+ */
+
+public class SignUpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        Window window = this.getWindow();
+
+// clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+// add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+// finally change the color
+        if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.LOLLIPOP){
+            window.setStatusBarColor(ContextCompat.getColor(this,R.color.my_statusbar_color));
+        }
+
+    }
+}
