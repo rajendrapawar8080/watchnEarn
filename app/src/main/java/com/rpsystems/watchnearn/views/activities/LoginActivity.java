4ff9030acb46e6827334218cfb062b997e9e45c3
Rajendra <rajendra.pawar@neosofttech.com>
2017-04-04 08:50:08 +0530

    login pig icon

--- app/src/main/java/com/rpsystems/watchnearn/views/activities/LoginActivity.java
+++ app/src/main/java/com/rpsystems/watchnearn/views/activities/LoginActivity.java
@@ -1,9 +1,15 @@
 package com.rpsystems.watchnearn.views.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import android.widget.EditText;
@@ -27,6 +33,11 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(ContextCompat.getColor(this,R.color.my_statusbar_color));
+        }
     }
     @OnClick(R.id.btn_login_id)public void loginUser(){
         startActivity(new Intent(this,HomeActivity.class));
